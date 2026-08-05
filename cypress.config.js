3dd23c428ddd333629a7279752c31130960dace5
Nicollas <nicollasgoncalvesdemoraes@gmail.com>
2026-08-05 12:17:43 -0300

    allure v3

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 const { allureCypress } = require('allure-cypress/reporter')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
       allureCypress(on, config, {
